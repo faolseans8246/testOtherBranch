e0caf90daa23f00fc7319270bbdf730021b0806e
acer NITRO 5 <feruzbekhamrayev2002@gmail.com>
2024-07-24 23:56:55 +0500

    Ikkinchi bosqich ulandi

--- csControl.js
+++ csControl.js
@@ -3,4 +3,9 @@ function vcFunc() {
     return "Mana shu dasturda ishlash davomida version controll amalga oshiriluvchi start func"
 }
 
-vcFunc()
+function ikkinchiFunc() {
+    return "Ikkinchi nitro qushilish"
+}
+
+
+console.log(vcFunc())
